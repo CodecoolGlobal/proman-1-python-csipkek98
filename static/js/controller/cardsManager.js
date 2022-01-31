87b9f68cad1b54559896ce93f0bb0b13b6e45d93
Csipke Krisztián <csipkekrisztian@gmail.com>
2022-01-31 16:18:04 +0100

    Add: css design base

--- static/js/controller/cardsManager.js
+++ static/js/controller/cardsManager.js
@@ -5,7 +5,6 @@ import { domManager } from "../view/domManager.js";
 export let cardsManager = {
   loadCards: async function (boardId) {
     const cards = await dataHandler.getCardsByBoardId(boardId);
-    console.log(cards)
     for (let card of cards) {
       const cardBuilder = htmlFactory(htmlTemplates.card);
       const content = cardBuilder(card);
