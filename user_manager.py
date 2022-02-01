c8729b710efeb17dfab6d33d4652122faed9815f
wdattila <wdattila@gmail.com>
2022-02-01 12:28:55 +0100

    Add: user insert to database testing

--- user_manager.py
+++ user_manager.py
@@ -1,5 +1,9 @@
 import data_manager
 import bcrypt
+from _datetime import datetime as dt
+
+
+DATA = {"name": "test", "password": "test", "email": "test"}
 
 
 def hash_password(plain_text_password):
@@ -10,3 +14,17 @@ def hash_password(plain_text_password):
 def verify_password(plain_text_password, hashed_password):
     hashed_bytes_password = hashed_password.encode('utf-8')
     return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_bytes_password)
+
+
+def register_user(data):
+    data["registered"] = dt.now().strftime("%Y-%m-%d %H:%M:%S")
+    data["password"] = hash_password(data["password"])
+    query = """
+    INSERT INTO users 
+    VALUES (nextval('users_id_seq'), %(name)s, %(password)s, %(registered)s, %(email)s);
+    """
+    data_manager.execute_query(query, data)
+
+
+if __name__ == "__main__":
+    register_user(DATA)
