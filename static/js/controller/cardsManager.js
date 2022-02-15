e2f0cdbfc81bee49065362fbc0cdd14ffac2420e
Csipke Krisztián <csipkekrisztian@gmail.com>
2022-02-15 13:45:17 +0100

    Add: refresh board when new card added

--- static/js/controller/cardsManager.js
+++ static/js/controller/cardsManager.js
@@ -2,6 +2,7 @@ import { dataHandler } from "../data/dataHandler.js";
 import { htmlFactory, htmlTemplates } from "../view/htmlFactory.js";
 import { domManager } from "../view/domManager.js";
 import * as dragAndDrop from "../view/dragAndDrop.js"
+import { reloadBoardData } from "../view/boardRefresh.js";
 
 export let cardsManager = {
   loadCards: async function (boardId) {
@@ -45,10 +46,10 @@ export let cardsManager = {
     const boardId = clickEvent.target.dataset.boardId;
     let cardTitle = document.querySelector(`.card-title-input[data-board-id="${boardId}"]`).value
     dataHandler.createNewCard(cardTitle, boardId)
+    reloadBoardData(boardId)
     document.querySelector(`.save-card[data-board-id="${boardId}"]`).hidden = true
     document.querySelector(`.card-title-input[data-board-id="${boardId}"]`).hidden = true
     document.querySelector(`.board-add[data-board-id="${boardId}"]`).style.display = 'block'
-    location.reload()
   }
 
 };
