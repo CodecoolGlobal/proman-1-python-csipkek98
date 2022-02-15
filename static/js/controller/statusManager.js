0212b68097752635461302b34f1b1eeae2e8c2cf
Nataniel Latorre <latorre.nataniel@hotmail.com>
2022-02-15 12:17:27 +0100

    Add reset on clickout of column names

--- static/js/controller/statusManager.js
+++ static/js/controller/statusManager.js
@@ -43,29 +43,44 @@ loadColumns : async function (boardId) {
       //     renameColumn
       //     );
       domManager.addEventListener(
-        `.board-column[data-column-id="${status.id}"]`,
+        `.board-column[data-column-id="${status.id}"] input`,
         "keydown",
           renameColumn
           );
+      domManager.addEventListener(
+          `.board-column[data-column-id="${status.id}"] input`,
+          "focusout",
+          resetForm
+      )
     }
   }
   }
 
 
-  async function deleteColumn(clickEvent){
-  let click = clickEvent.target.parentElement
-  if (click.classList.contains("board-column-remove")){
-    let columnId = click.parentElement.getAttribute("data-column-id")
-    await dataHandler.deleteStatus(columnId)
-    click.parentElement.remove()
+async function deleteColumn(clickEvent){
+    let click = clickEvent.target.parentElement
+    if (click.classList.contains("board-column-remove")){
+        let columnId = click.parentElement.getAttribute("data-column-id")
+        await dataHandler.deleteStatus(columnId)
+        click.parentElement.remove()
     }
 }
 
 async function renameColumn(clickEvent){
-  const statusIdd = clickEvent.target.dataset.statusIdd;
-  const newTitle = document.querySelector(`input[data-status-idd="${statusIdd}"]`);
-  if (clickEvent.key === 'Enter'){
-    await dataHandler.renameColumn(newTitle.value, statusIdd);
-    newTitle.readOnly === true ? newTitle.readOnly = false : newTitle.readOnly = true;
+    const targetInput = clickEvent.target;
+    targetInput.readOnly === true ? targetInput.readOnly = false : targetInput.readOnly = true;
+    targetInput.onfocus = this.selectionStart = this.selectionEnd = this.value.length;
+    const statusIdd = clickEvent.target.dataset.statusIdd;
+    const newTitle = document.querySelector(`input[data-status-idd="${statusIdd}"]`);
+    if (clickEvent.key === 'Enter'){
+        await dataHandler.renameColumn(newTitle.value, statusIdd);
+        newTitle.readOnly === true ? newTitle.readOnly = false : newTitle.readOnly = true;
+    }
 }
+
+function resetForm() {
+  let allForms = document.querySelectorAll("form");
+  for (let form of allForms) {
+    form.reset()
+  }
 }
